cbc50f9d4fe73eeaa2f02e0e4f936bfc4f2307cb
Iman Jalali <i.jalali88@gmail.com>
2025-08-01 23:15:35 +0330

    create Navbar

--- app/about/team/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function TeamPage() {
-  return (
-    <div className='text-white text-5xl'>team page inside the AboutPage</div>
-  )
-}
-
-export default TeamPage;
---
+++ components/home/Navbar/MobileNav.tsx
@@ -0,0 +1,45 @@
+import { NavLinks } from "@/constant/constant";
+import Link from "next/link";
+import React from "react";
+import { CgClose } from "react-icons/cg";
+
+type Props = {
+  showNav: boolean;
+  closeNav: () => void;
+};
+
+function MobileNav({ closeNav, showNav }: Props) {
+  const navOpen = showNav ? "translate-x-0" : "translate-x-[100%]";
+
+  return (
+    <div>
+      {/* overlay */}
+      <div
+        className={`fixed inset-0 ${
+          showNav ? "opacity-70" : "opacity-0 pointer-events-none"
+        } transition-opacity duration-300 z-[100002] bg-black w-full h-screen`}
+        onClick={closeNav}
+      ></div>
+
+      {/* navLinks */}
+      <div
+        className={`text-white ${navOpen} fixed justify-center flex flex-col h-full transform transition-all duration-500 delay-300 w-[80%] sm:w-[60%] bg-cyan-800 space-y-6 z-[100050] right-0`}
+      >
+        {NavLinks.map((link) => (
+          <Link key={link.id} href={link.url}>
+            <p className="text-white w-fit text-xl ml-12 border-b-[1.5px] pb-1 border-white sm:text-[30px]">
+              {link.label}
+            </p>
+          </Link>
+        ))}
+        {/* cross icon */}
+        <CgClose
+          onClick={closeNav}
+          className="absolute top-[0.7rem] right-[1.4rem] sm:w-8 sm:h-8 w-6 h-6 cursor-pointer"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default MobileNav;
