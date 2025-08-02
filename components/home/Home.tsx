8678316c6c674e0d8bdfe7d6773b23e7f4552baf
Iman Jalali <i.jalali88@gmail.com>
2025-08-02 11:11:00 +0330

    create services section

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Hero from './Hero/Hero'
+import Services from './Services/Services'
 
 function Home() {
   return (
-    <div className='overflow-hidden'>Home</div>
+    <div className='overflow-hidden'>
+      <Hero />
+      <Services />
+    </div>
   )
 }
 
