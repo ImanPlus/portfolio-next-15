b1b6a0079db65e53239c65e8d4f303ef8f3c26a2
Iman Jalali <i.jalali88@gmail.com>
2025-08-02 13:07:10 +0330

    create resume section

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './Hero/Hero'
 import Services from './Services/Services'
+import Resume from './Resume/Resume'
 
 function Home() {
   return (
     <div className='overflow-hidden'>
       <Hero />
       <Services />
+      <Resume />
     </div>
   )
 }
--- components/home/Services/Services.tsx
+++ components/home/Services/Services.tsx
@@ -8,7 +8,7 @@ const Services = () => {
         Colleborate with brand <br /> and agencies to create <br /> impactful
         results
       </h1>
-      <div className="w-90 sm:w-[70%] mx-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-10 items-center">
+      <div className="w-[90%] sm:w-[70%] mx-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-10 items-center mt-20">
         <ServiceCard
           icon="/images/s1.png"
           name="UI and UX"
