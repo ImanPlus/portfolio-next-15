2582e17a6d681490787efd2057e65a587df386f3
Iman Jalali <i.jalali88@gmail.com>
2025-08-02 16:53:40 +0330

    create ClientReview Section

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -4,6 +4,7 @@ import Services from './Services/Services'
 import Resume from './Resume/Resume'
 import Projects from './Projects/Projects'
 import Skills from './Skills/Skills'
+import ClientReview from './ClientReview/ClientReview'
 
 function Home() {
   return (
@@ -13,6 +14,7 @@ function Home() {
       <Resume />
       <Projects />
       <Skills />
+      <ClientReview />
     </div>
   )
 }
