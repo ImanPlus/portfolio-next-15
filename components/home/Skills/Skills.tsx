40973f6d5ad2c0e723b1644b9352b0d1832c2119
Iman Jalali <i.jalali88@gmail.com>
2025-08-17 10:19:09 +0330

    remove percentage

--- components/home/Skills/Skills.tsx
+++ components/home/Skills/Skills.tsx
@@ -16,22 +16,17 @@ const skills = [
   {
     name: "JavaScript",
     icon: <SiJavascript />,
-    percentage: 89,
+    // percentage: 89,
   },
   {
     name: "React.js",
     icon: <SiReact />,
-    percentage: 92,
+    // percentage: 92,
   },
   {
     name: "Next.js",
     icon: <SiNextdotjs />,
-    percentage: 90,
-  },
-  {
-    name: "Node.js",
-    icon: <SiNodedotjs />,
-    percentage: 88,
+    // percentage: 90,
   },
   {
     name: "Tailwind Css",
@@ -52,7 +47,7 @@ function Skills() {
         My <span className="text-cyan-300">Skills</span>
       </h1>
       <div className="flex flex-wrap justify-center gap-6 mt-16">
-        {skills.map((skill,i) => {
+        {skills.map((skill, i) => {
           return (
             <Tilt key={skill.name} scale={1.5} transitionSpeed={400}>
               <div
@@ -62,7 +57,7 @@ function Skills() {
                 className="bg-[#14134145] text-center w-40 h-48 rounded-e-3xl flex flex-col items-center justify-center shadow-lg transition hover:scale-105"
               >
                 <div className="text-5xl mb-4 text-gray-300">{skill.icon}</div>
-                <p className="text-2xl font-semibold">{skill.percentage}%</p>
+                {/* <p className="text-2xl font-semibold">{skill.percentage}%</p> */}
                 <p className="text-purple-400 mt-1">{skill.name}</p>
               </div>
             </Tilt>
